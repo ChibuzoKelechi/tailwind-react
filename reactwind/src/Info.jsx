c024a10676ed7d89019ae6def78529fe5880fe83
KelechiCV <narutokelechi@gmail.com>
2023-09-13 22:43:46 +0100

    Lang components added

--- reactwind/src/Info.jsx
+++ reactwind/src/Info.jsx
@@ -1,9 +1,27 @@
 const Info = () => {
     return ( 
         <section className="">
-         
+         <Lang 
+           title='Javascript' 
+           desc='A programming language built primarily to give functionality to web pages, but now used in other fields like mobile development'
+        />
+
+         <Lang 
+           title='Python' 
+           desc='A high level dynamic programming language used in many fields like machine learning, data science, etc'
+        />
         </section>
      );
 }
  
 export default Info;
+
+// eslint-disable-next-line react/prop-types
+const Lang = ({title, desc}) => {
+    return ( 
+        <article className="m-3 p-3">
+         <h2 className="text-2xl font-bold">{title}</h2>
+         <p className="py-1 font-medium">{desc}</p>
+        </article>
+     );
+}
